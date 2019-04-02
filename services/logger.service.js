94e9ec949c03a6f938fb625be0083e976d678db8
moshe-pinhasi <pi.moshe@gmail.com>
2019-04-03 00:02:58 +0300

    displaying the logs in logzio

--- services/logger.service.js
+++ services/logger.service.js
@@ -2,41 +2,47 @@ const fs = require('fs')
 const { createLogger, format, transports } = require('winston')
 const { combine, timestamp, label, prettyPrint, printf } = format
 
+const LogzioWinstonTransport = require('winston-logzio');
+const logzioWinstonTransport = new LogzioWinstonTransport({
+  level: 'debug',
+  name: 'api-server_winston_logzio',
+  token: 'wfhCIwrtYiIHLKYZnWDxuJswOENHIpZJ',
+  host: 'listener.logz.io',
+});
+
 const logsdir = './logs';
 if (!fs.existsSync(logsdir)) {
   fs.mkdirSync(logsdir);
 }
 
 //define the time format
-const timeFormatFn = () => {
-  let now = new Date();
-  return now.toUTCString();
-};
+const timeFormatFn = () => (new Date()).toUTCString();
 
 const logger = createLogger({
-  level: 'info',
+  level: 'debug',
   format: combine(
-    label({ label: 'node-js' }),
+    label({ label: 'nodejs' }),
     timestamp({
       format: timeFormatFn
     }),
     prettyPrint()
   ),
 
-  defaultMeta: { service: 'nodejs-service' },
+  defaultMeta: { service: 'nodejs-api-service' },
   transports: [
+    logzioWinstonTransport,
     // - Write to all logs with level `info` and below to `combined.log` 
     // - Write all logs error (and below) to `error.log`.
     new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' })
+    new transports.File({ filename: 'logs/combined.log' }),
   ]
 })
 
 if (process.env.NODE_ENV !== 'production') {
 
-  const simpleFormat = printf(({ level, message, label, timestamp }) =>
-    `[${timestamp}] ${level.toUpperCase()} - ${message}`)
-
+  const simpleFormat = printf(({ level, message, label, service, timestamp }) =>
+    `[${timestamp}] [${service}] ${level.toUpperCase()} - ${message}`)
+  
   logger.add(new transports.Console({
     format: simpleFormat,
     level: 'debug',
