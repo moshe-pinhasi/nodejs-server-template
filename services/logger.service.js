50243353c715a4060baf837c75c8296fc0ad60d6
moshe-pinhasi <pi.moshe@gmail.com>
2019-06-09 23:49:00 +0300

    fixes in logger

--- services/logger.service.js
+++ services/logger.service.js
@@ -4,59 +4,56 @@ const { combine, timestamp, label, prettyPrint, printf } = format
 
 const logsdir = './logs';
 if (!fs.existsSync(logsdir)) {
-  fs.mkdirSync(logsdir);
+    fs.mkdirSync(logsdir);
 }
 
 //define the time format
 const timeFormatFn = () => {
-  let now = new Date();
-  return now.toUTCString();
+    let now = new Date();
+    return now.toUTCString();
 };
 
 const logger = createLogger({
-  level: 'info',
-  format: combine(
-    label({ label: 'node-js' }),
-    timestamp({
-      format: timeFormatFn
-    }),
-    prettyPrint()
-  ),
+    level: 'info',
+    format: combine(
+        label({ label: process.env.NODE_ENV }),
+        timestamp({
+            format: timeFormatFn
+        }),
+        prettyPrint()
+    ),
 
-  defaultMeta: { service: 'nodejs-service' },
-  transports: [
-    // - Write to all logs with level `info` and below to `combined.log` 
-    // - Write all logs error (and below) to `error.log`.
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' })
-  ]
+    defaultMeta: { service: 'nodejs-api-service' }, // change it to the service name
+    transports: [
+        new transports.File({ filename: 'logs/log.log' })
+    ]
 })
 
 if (process.env.NODE_ENV !== 'production') {
 
-  const simpleFormat = printf(({ level, message, label, timestamp }) =>
-    `[${timestamp}] ${level.toUpperCase()} - ${message}`)
+    const simpleFormat = printf(({ level, message, label, timestamp }) =>
+        `[${timestamp}] ${level.toUpperCase()} - ${message}`)
 
-  logger.add(new transports.Console({
-    format: simpleFormat,
-    level: 'debug',
-  }))
+    logger.add(new transports.Console({
+        format: simpleFormat,
+        level: 'debug',
+    }))
 }
 
 module.exports = {
-  debug: (message, meta = "") => {
-    logger.log("debug", message, meta)
-  },
+    debug: (message, meta = "") => {
+        logger.log("debug", message, meta)
+    },
 
-  info: (message, meta) => {
-    logger.log("info", message, meta)
-  },
+    info: (message, meta) => {
+        logger.log("info", message, meta)
+    },
 
-  warn: (message, meta) => {
-    logger.log("warn", message, meta)
-  },
+    warn: (message, meta) => {
+        logger.log("warn", message, meta)
+    },
 
-  error:(message, meta) => {
-    logger.log("error", message, meta)
-  }
+    error: (message, meta) => {
+        logger.log("error", message, meta)
+    }
 }
